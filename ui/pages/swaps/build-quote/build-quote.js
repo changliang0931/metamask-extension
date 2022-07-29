bd9419ccd6b475ce4e11f65c4a7b652e1e5a075f
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2022-07-29 17:07:42 -0400

    fix unit test fail

--- ui/pages/swaps/build-quote/build-quote.js
+++ ui/pages/swaps/build-quote/build-quote.js
@@ -66,7 +66,6 @@ import {
   getCurrentChainId,
   getRpcPrefsForCurrentProvider,
   getUseTokenDetection,
-  getTokenList,
   isHardwareWallet,
   getHardwareWalletType,
 } from '../../../selectors';
@@ -150,7 +149,6 @@ export default function BuildQuote({
   const defaultSwapsToken = useSelector(getSwapsDefaultToken, isEqual);
   const chainId = useSelector(getCurrentChainId);
   const rpcPrefs = useSelector(getRpcPrefsForCurrentProvider, shallowEqual);
-  const tokenList = useSelector(getTokenList, isEqual);
   const useTokenDetection = useSelector(getUseTokenDetection);
   const quotes = useSelector(getQuotes, isEqual);
   const areQuotesPresent = Object.keys(quotes).length > 0;
@@ -212,7 +210,7 @@ export default function BuildQuote({
     conversionRate,
     currentCurrency,
     chainId,
-    tokenList,
+    shuffledTokensList,
     useTokenDetection,
   );
 
