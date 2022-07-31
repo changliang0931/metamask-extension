90e52d4430429f25ac31af716fe7e07a1f415999
brad-decker <bhdecker84@gmail.com>
2022-07-31 17:36:01 -0500

    remove unnecessary plugins

--- babel.config.js
+++ babel.config.js
@@ -12,10 +12,5 @@ module.exports = function (api) {
       '@babel/preset-env',
       '@babel/preset-react',
     ],
-    plugins: [
-      '@babel/plugin-proposal-object-rest-spread',
-      '@babel/plugin-proposal-optional-chaining',
-      '@babel/plugin-proposal-nullish-coalescing-operator',
-    ],
   };
 };
